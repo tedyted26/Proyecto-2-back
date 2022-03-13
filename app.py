ee91b1139f31e63f7cd6638dcdf9db7223c5c062
Andres Perez <afperez772@gmail.com>
2022-03-13 19:11:54 +0100

    Funcion CORS agregada

--- app.py
+++ app.py
@@ -2,30 +2,13 @@
 #External Imports
 from flask import Flask, request, jsonify
 from textblob import TextBlob
+from flask_cors import CORS, cross_origin
 
 #Internal Imports
 from helpers import *
 
 app = Flask(__name__)
-
-books = [
-    {'id': 0,
-     'title': 'A Fire Upon the Deep',
-     'author': 'Vernor Vinge',
-     'first_sentence': 'The coldsleep itself was dreamless.',
-     'year_published': '1992'},
-    {'id': 1,
-     'title': 'The Ones Who Walk Away From Omelas',
-     'author': 'Ursula K. Le Guin',
-     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
-     'published': '1973'},
-    {'id': 2,
-     'title': 'Dhalgren',
-     'author': 'Samuel R. Delany',
-     'first_sentence': 'to wound the autumnal city.',
-     'published': '1975'}
-]
-
+CORS(app)
 
 
 @app.route('/')
