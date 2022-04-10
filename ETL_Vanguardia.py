89e4f4d380720e42907fb6555be0d10ed95300bb
tedyted26 <tedyted26@hotmail.com>
2022-04-10 16:22:22 +0200

    me queda aceptar las cookies con selenium

--- ETL_Vanguardia.py
+++ ETL_Vanguardia.py
@@ -1,8 +1,14 @@
 from bs4 import BeautifulSoup
 from urllib import request as rq
-from Noticia import Noticia 
+from Noticia import Noticia
+# pip install selenium
+# pip install webdriver_manager
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
 import ssl
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
 
 categoria = "salamanca"
 urlbase = "https://stories.lavanguardia.com/search?q="
@@ -48,26 +54,38 @@ if html != None:
     # dentro de las noticia
     for article in resultados:
         url_art = article.find("a")["href"]
-
+        # esta url es solo de prueba
+        url_art = "https://www.lavanguardia.com/internacional/20220408/8185819/rusia-excluida-consejo-derechos-humanos-onu.html"
         html_art = rq.urlopen(url_art, context=ssl.SSLContext()).read()
         soup_art = BeautifulSoup(html_art, 'html.parser')
 
         titulo = soup_art.find(class_="title").text
+
         subtitulos = soup_art.find_all(class_="epigraph")
         subtitulo = ""
         for sub in subtitulos:
             subtitulo = subtitulo + sub.text
-        fecha = soup_art.find("time")["datetime"]
-        categoria = soup_art.find(class_="supra-title").text
-        periodico = "La Vanguardia"
-        tags = ""
 
-        # para ver los comentarios: <div class="spotim-open comments-bbd14976-532c-11eb-acb3-c65f349e9d3e-20210110112042">Mostrar comentarios</div> Comprobar que los numeros estan bien
-        # <ul class="spcv_messages-list"
+        fecha = soup_art.find("time")["datetime"]
+
+        categoria = soup_art.find(class_="supra-title").text
+
+        periodico = "La Vanguardia"
+
+        tags = ""
 
         texto_entero = soup_art.find(class_="article-modules").find_all("p")
         texto = ""
         for p in texto_entero:
             texto = texto + p.text
+
+        driver.get(url_art)
+
+        #cart = driver.find_element_by_class_name("spotim-open")
+        #cart.click()
+        #modal_comentarios = soup_art.find(class_="comments-modal") # spcv_messages-list
+        #print(modal_comentarios.prettify())
+        #ul_comentarios = modal_comentarios.find("ul")
+        #lista_comentarios = ul_comentarios.find_all(class_="spcv_list-item")
 
 
